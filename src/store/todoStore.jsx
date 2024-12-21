7f748326f9dded1f0c08408f2ad0dc8752ee00a1
Arman Rajib <ahrajib6789@gmail.com>
2024-12-22 01:13:52 +0600

    Integrate devtools (redux devtools) in the zustand store

--- src/store/todoStore.jsx
+++ src/store/todoStore.jsx
@@ -1,27 +1,30 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useTodoStore = create((set) => ({
-  todos: [],
+const useTodoStore = create(
+  devtools((set) => ({
+    todos: [],
 
-  // Add a new todo
-  addTodo: (text) =>
-    set((state) => ({
-      todos: [...state.todos, { id: Date.now(), text, completed: false }],
-    })),
+    // Add a new todo
+    addTodo: (text) =>
+      set((state) => ({
+        todos: [...state.todos, { id: Date.now(), text, completed: false }],
+      })),
 
-  // Remove a todo
-  removeTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== id),
-    })),
+    // Remove a todo
+    removeTodo: (id) =>
+      set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== id),
+      })),
 
-  // Toggle a todo
-  toggleTodo: (id) =>
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ),
-    })),
-}));
+    // Toggle a todo
+    toggleTodo: (id) =>
+      set((state) => ({
+        todos: state.todos.map((todo) =>
+          todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ),
+      })),
+  }))
+);
 
 export default useTodoStore;
